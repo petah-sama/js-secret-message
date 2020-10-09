080bfc88dee96248170b9d94d319ba39d26fe27f
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-09 17:12:23 +0100

    simple app to share secret messages

---
+++ app.js
@@ -0,0 +1,32 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+});
+
+const copyText = () => {
+    const linkInput = document.querySelector('#link-input');
+    linkInput.select();
+    linkInput.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand('copy');
+}
